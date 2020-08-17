fb614b76146126559be77e0c2c3e49ef936a7749
Jan Paolo Go <go.janpaolo@gmail.com>
2020-08-17 13:07:35 -0500

    add console app

---
+++ WebApplication1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("my console app");
+            Console.ReadLine();
+        }
+    }
+}
